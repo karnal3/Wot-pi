72298e066d68289a2a3a0aff947003eb23a67390
karnal3 <46356692+karnal3@users.noreply.github.com>
2019-11-23 21:04:58 +0100

    Add files via upload

---
+++ routes/things.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+  router = express.Router(),
+  resources = require('./../resources/model');
+
+router.route('/coapDevice/sensors/:id').get(function (req, res, next) {
+  var id = req.params.id;
+  req.result = resources.things.coapDevice.sensors[id];
+  next();
+});
+
+router.route('/coapDevice/sensors/co2').get(function (req, res, next) {
+  req.result = resources.things.coapDevice.sensors.co2;
+  next();
+});
+
+router.route('/coapDevice/sensors/temp').get(function (req, res, next) {
+  req.result = resources.things.coapDevice.sensors.temp;
+  next();
+});
+
+
+module.exports = router;
